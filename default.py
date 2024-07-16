6c36f76ebb2fe1fca3073dedcd4a28dd19c89f05
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-07-16 12:15:15 +0100

    black for default code

--- default.py
+++ default.py
@@ -19,24 +19,20 @@ import adafruit_display_shapes.circle
 # we can create ours
 displayio.release_displays()
 matrix = rgbmatrix.RGBMatrix(
-    width=128, bit_depth=4,
+    width=128,
+    bit_depth=4,
     rgb_pins=[
         board.MTX_R1,
         board.MTX_G1,
         board.MTX_B1,
         board.MTX_R2,
         board.MTX_G2,
-        board.MTX_B2
+        board.MTX_B2,
     ],
-    addr_pins=[
-        board.MTX_ADDRA,
-        board.MTX_ADDRB,
-        board.MTX_ADDRC,
-        board.MTX_ADDRD
-    ],
+    addr_pins=[board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD],
     clock_pin=board.MTX_CLK,
     latch_pin=board.MTX_LAT,
-    output_enable_pin=board.MTX_OE
+    output_enable_pin=board.MTX_OE,
 )
 display = framebufferio.FramebufferDisplay(matrix)
 
@@ -47,15 +43,17 @@ display = framebufferio.FramebufferDisplay(matrix)
 # but if you change the font you might find that other values work better.
 line1 = adafruit_display_text.label.Label(
     terminalio.FONT,
-    color=0xffFFFF,
-    text="This scroller is brought to you by CircuitPython RGBMatrix")
+    color=0xFFFFFF,
+    text="This scroller is brought to you by CircuitPython RGBMatrix",
+)
 line1.x = display.width
 line1.y = 6
 
 line2 = adafruit_display_text.label.Label(
     terminalio.FONT,
-    color=0x0080ff,
-    text="Hello to all CircuitPython contributors worldwide <3")
+    color=0x0080FF,
+    text="Hello to all CircuitPython contributors worldwide <3",
+)
 line2.x = display.width
 line2.y = 24
 
@@ -64,6 +62,7 @@ g = displayio.Group()
 g.append(line1)
 g.append(line2)
 display.root_group = g
+
 
 # This function will scoot one label a pixel to the left and send it back to
 # the far right if it's gone all the way off screen. This goes in a function
@@ -74,6 +73,7 @@ def scroll(line):
     if line.x < -line_width:
         line.x = display.width
 
+
 # This function scrolls lines backwards.  Try switching which function is
 # called for line2 below!
 def reverse_scroll(line):
@@ -82,11 +82,11 @@ def reverse_scroll(line):
     if line.x >= display.width:
         line.x = -line_width
 
+
 # You can add more effects in this loop. For instance, maybe you want to set the
 # color of each label to a different value.
 while True:
     scroll(line1)
     scroll(line2)
-    #reverse_scroll(line2)
+    # reverse_scroll(line2)
     display.refresh(minimum_frames_per_second=0)
-
